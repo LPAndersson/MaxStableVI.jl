2de02211707e964c973a245ce5d2df042d0f580c
Patrik Andersson <patrik@ndersson.nu>
2023-05-24 16:40:05 +0200

    fix

--- src/test2.jl
+++ src/test2.jl
@@ -1,6 +1,7 @@
 using MaxStableVI
 using Flux
 import Optim
+using Plots
 
 # generate some test data
 D = 5
@@ -11,11 +12,25 @@ observations = sample(
     coordinates = coordinates, 
     n = 10
     )
+
 data = [observations, coordinates]
 #model = LogisticModel( theta = 0.5)
 
 model = BrownResnickModel(lambda = 0.5, nu = 0.5)
 compositeMle!(model, data = data, degree = 2)
 
-model = BrownResnickModel(lambda = 0.5, nu = 0.5)
-mle!(model, data = data)
+# model = BrownResnickModel(lambda = 0.5, nu = 0.5)
+# mle!(model, data = data)
+
+loglik = function(lambda, nu)
+    modelCopy = deepcopy(model)
+
+
+    modelCopy.lambda = [lambda]
+    modelCopy.nu = [nu]
+    return compositeLogLikelihood(modelCopy, data, 2)
+end
+
+x = range(0, 2, length=100)
+y = loglik.(1.54,x)
+plot(x,y)
