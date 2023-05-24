ad7cd61e1588d62066a2c120d87791e314107644
Patrik Andersson <patrik@ndersson.nu>
2023-05-24 09:07:00 +0200

    change optim tolerance

--- src/test2.jl
+++ src/test2.jl
@@ -3,19 +3,19 @@ using Flux
 import Optim
 
 # generate some test data
-D = 50
+D = 5
 coordinates = rand(D,2)
 observations = sample(
-    #BrownResnickModel(lambda = 0.5, nu = 0.5), 
-    LogisticModel(theta =  0.3),
+    BrownResnickModel(lambda = 0.5, nu = 0.5), 
+    #LogisticModel(theta =  0.3),
     coordinates = coordinates, 
-    n = 20
+    n = 10
     )
 data = [observations, coordinates]
-model = LogisticModel( theta = 0.5)
+#model = LogisticModel( theta = 0.5)
 
-#model = BrownResnickModel(lambda = 0.5, nu = 0.5)
+model = BrownResnickModel(lambda = 0.5, nu = 0.5)
 compositeMle!(model, data = data, degree = 5)
 
-model = LogisticModel( theta = 0.5)
-mle!(model, data = data)
+#model = LogisticModel( theta = 0.5)
+#mle!(model, data = data)
