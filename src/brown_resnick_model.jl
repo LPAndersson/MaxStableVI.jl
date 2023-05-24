1f288d9dadbe3fae84fc1fb2da41fef753d1adb0
Patrik Andersson <patrik@ndersson.nu>
2023-05-24 09:45:17 +0200

    fix

--- src/brown_resnick_model.jl
+++ src/brown_resnick_model.jl
@@ -233,8 +233,8 @@ function mle!(model::BrownResnickModel; data::Vector{Matrix{Float64}})
     modelCopy = deepcopy(model)
 
     function loss(x)
-        modelCopy.lambda = softplus.([x[1]]) + 0.01
-        modelCopy.nu = 1.98.*logistic.([x[2]]) + 0.01
+        modelCopy.lambda = softplus.([x[1]]) .+ 0.01
+        modelCopy.nu = 1.98.*logistic.([x[2]]) .+ 0.01
         return -loglikelihoodEnumerate(modelCopy, data)
     end
 
@@ -250,8 +250,8 @@ function mle!(model::BrownResnickModel; data::Vector{Matrix{Float64}})
             )
         )
     
-    model.lambda =  [softplus.([x[1]]) + 0.01]
-    model.nu = [1.98.*logistic.([x[2]]) + 0.01]
+    model.lambda =  [softplus.([x[1]]) .+ 0.01]
+    model.nu = [1.98.*logistic.([x[2]]) .+ 0.01]
 
     return model
 
@@ -262,8 +262,8 @@ function compositeMle!(model::BrownResnickModel; data::Vector{Matrix{Float64}}, 
     modelCopy = deepcopy(model)
 
     loss = function(x::Vector{Float64})
-        modelCopy.lambda = softplus.([x[1]]) + 0.01
-        modelCopy.nu = 1.98.*logistic.([x[2]]) + 0.01
+        modelCopy.lambda = softplus.([x[1]]) .+ 0.01
+        modelCopy.nu = 1.98.*logistic.([x[2]]) .+ 0.01
         return -compositeLogLikelihood(modelCopy, data, degree)
     end
 
@@ -279,8 +279,8 @@ function compositeMle!(model::BrownResnickModel; data::Vector{Matrix{Float64}}, 
             )
         )
     
-    model.lambda =  [softplus.([x[1]]) + 0.01]
-    model.nu = [1.98.*logistic.([x[2]]) + 0.01]
+    model.lambda =  [softplus.([x[1]]) .+ 0.01]
+    model.nu = [1.98.*logistic.([x[2]]) .+ 0.01]
 
             # optimal = Optim.optimize(
     #     loss, 
