7f81cdd92b86f56cfad653dbeb5e069c6fc63eed
Patrik Andersson <patrik@ndersson.nu>
2023-09-27 14:25:01 +0200

    add loglikehood

--- src/neural_clustering.jl
+++ src/neural_clustering.jl
@@ -55,7 +55,6 @@ function sample(
     g = guide.g
     f = guide.f
 
-
     N, dh = size(h)
 
     c = Vector{Int32}(undef,N)
@@ -109,3 +108,52 @@ sample(
     partition,
     obsNum
     )
+
+
+function logLikelihood(
+    guide::NeuralClustering, 
+    observation::Vector{Float64},
+    coordinate::Matrix{Float64},
+    partition::Vector{Vector{Int64}},
+    obsNum::Int64
+    )
+    
+    h = guide.h
+    u = guide.u
+    g = guide.g
+    f = guide.f
+
+    N, dh = size(h)
+
+    c = Vector{Int32}(undef,N)
+
+    for (index,value) in enumerate(partition)
+        for cust in value
+            c[cust] = index
+        end
+    end
+    
+    logLikelihood = 0.0
+
+    U = vec(sum(u[2:end,:],dims = 1))
+    H = [h[1:1,:];zeros(Float32,N-1,dh)]
+
+    G = g(H[1,:])
+    K = 1
+    Zygote.@ignore c[1] = 1
+
+    for n in 2:N
+        U = U - u[n,:]
+
+        log_q = SliceMap.slicemap((x) -> f([G + g(x + h[n,:]) - g(x);U]), H[1:(K+1),:], dims = 2)
+        q = Flux.softmax(vec(log_q))
+        logLikelihood += log(copy(q[c[n]]))
+        if c[n] == K+1
+            K = K + 1
+        end
+        G = G .- g(H[c[n],:]) .+ g(H[c[n],:]+h[n,:])
+        H = H .+ [zeros(Float32,c[n]-1,dh);h[n:n,:];zeros(Float32,N-c[n],dh)]
+    end
+    
+    return logLikelihood
+end
